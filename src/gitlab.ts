7186ba90d92f57b884fccb30247b257e2fae4bc2
Jeremy Wells <jemmyw@gmail.com>
2021-05-06 00:10:41 +1200

    Initial commit

---
+++ src/gitlab.ts
@@ -0,0 +1,165 @@
+import { gql, GraphQLClient } from "graphql-request";
+
+const ENDPOINT = "https://gitlab.com/api/graphql";
+
+export async function authGitlab() {
+  return await aha.auth("gitlab", { useCachedRetry: true });
+}
+
+/**
+ * Create an authenticated graphql fetch function
+ *
+ * ```
+ * const api = await gitlabGraphql();
+ * await api(GraphqlQuery, {variables});
+ * ```
+ */
+export async function gitlabGraphql() {
+  const authData = await authGitlab();
+  const client = new GraphQLClient(ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${authData.token}`,
+    },
+  });
+
+  const request = <T>(...args: Parameters<GraphQLClient["request"]>) => {
+    return client.request<T>(...args);
+  };
+
+  return request;
+}
+
+interface ProjectFragment {
+  id: string;
+  name: string;
+  fullPath: string;
+}
+
+const ProjectFragment = gql`
+  fragment ProjectFragment on Project {
+    id
+    name
+    fullPath
+  }
+`;
+
+type ProjectMemberships = {
+  currentUser: {
+    projectMemberships: { nodes: { project: ProjectFragment }[] };
+  };
+};
+
+const ProjectMemberships = gql`
+  query ProjectMemberships($limit: Int!) {
+    currentUser {
+      projectMemberships(first: $limit) {
+        nodes {
+          project {
+            ...ProjectFragment
+          }
+        }
+      }
+    }
+  }
+
+  ${ProjectFragment}
+`;
+
+type SearchProjects = { projects: { nodes: ProjectFragment[] } };
+
+const SearchProjects = gql`
+  query SearchProjects($term: String!, $limit: Int!) {
+    projects(membership: true, search: $term, first: $limit) {
+      nodes {
+        ...ProjectFragment
+      }
+    }
+  }
+
+  ${ProjectFragment}
+`;
+
+interface ProjectIssueEdge {
+  cursor: string;
+  node: {
+    id: string;
+    iid: string;
+    title: string;
+    descriptionHtml: string;
+    webUrl: string;
+  };
+}
+
+type ProjectIssues = {
+  project: {
+    issues: {
+      edges: ProjectIssueEdge[];
+      pageInfo: { endCursor: string };
+    };
+  };
+};
+
+const ProjectIssues = gql`
+  query ProjectIssues($path: ID!, $cursor: String) {
+    project(fullPath: $path) {
+      issues(first: 20, after: $cursor) {
+        edges {
+          cursor
+          node {
+            id
+            iid
+            title
+            descriptionHtml
+            webUrl
+          }
+        }
+        pageInfo {
+          endCursor
+        }
+      }
+    }
+  }
+`;
+
+/**
+ * Search for projects by query from the current users memberships
+ */
+export async function autocompleteProject(
+  query: string
+): Promise<Aha.FilterValue[]> {
+  const api = await gitlabGraphql();
+  let projects: ProjectFragment[] = [];
+
+  if (query.length === 0) {
+    projects = (
+      await api<ProjectMemberships>(ProjectMemberships, { limit: 30 })
+    ).currentUser.projectMemberships.nodes.map((node) => node.project);
+  } else {
+    projects = (
+      await api<SearchProjects>(SearchProjects, { term: query, limit: 30 })
+    ).projects.nodes;
+  }
+
+  return projects.map((project) => ({
+    text: project.name,
+    value: project.fullPath,
+  }));
+}
+
+/**
+ * Find issues in a given project
+ */
+export async function findIssues(projectPath: string, cursor?: string) {
+  const api = await gitlabGraphql();
+  const {
+    project: { issues },
+  } = await api<ProjectIssues>(ProjectIssues, {
+    path: projectPath,
+    cursor,
+  });
+
+  const records = issues.edges.map((edge) => edge.node);
+  const endCursor = issues.pageInfo.endCursor;
+
+  return { records, endCursor };
+}
---
+++ src/import.tsx
@@ -0,0 +1,58 @@
+import { authGitlab, autocompleteProject, findIssues } from "./gitlab";
+
+interface GitlabRecord extends Aha.ImportRecord {
+  description: string;
+}
+
+const importer = aha.getImporter<GitlabRecord>(
+  "aha-develop.gitlab-import.gitlab"
+);
+
+importer.on(
+  { action: "listFilters" },
+  async (): Promise<Aha.ListFilters> => {
+    authGitlab();
+
+    return {
+      project: {
+        title: "Project",
+        required: true,
+        type: "autocomplete",
+      },
+    };
+  }
+);
+
+importer.on({ action: "filterValues" }, async ({ filterName, filters }) => {
+  let values: Aha.FilterValue[] = [];
+
+  switch (filterName) {
+    case "project":
+      values = await autocompleteProject(filters[filterName]);
+  }
+
+  return values;
+});
+
+importer.on({ action: "listCandidates" }, async ({ filters, nextPage }) => {
+  if (!filters.project) return { records: [], nextPage };
+
+  const page = await findIssues(filters.project, nextPage);
+  const records = page.records.map<GitlabRecord>((issue) => ({
+    name: issue.title,
+    uniqueId: issue.id,
+    identifier: issue.iid,
+    url: issue.webUrl,
+    description: issue.descriptionHtml,
+  }));
+
+  return {
+    records,
+    nextPage: page.endCursor,
+  };
+});
+
+importer.on({ action: "importRecord" }, async ({ importRecord, ahaRecord }) => {
+  (ahaRecord as any).description = importRecord.description;
+  await ahaRecord.save();
+});
